14a226989968b42dd24c4a6a901bf5065579ea7f
danbulnet <daniel@bulanda.net>
2022-08-02 19:08:14 +0200

    fix & improve prediction algorithm

--- src/magds/Algorithms.jl
+++ src/magds/Algorithms.jl
@@ -2,76 +2,68 @@ module Algorithms
 
 using DataFrames
 
-import ..MAGDSParser: infertype
 using ..MAGDSSimple
+import ..MAGDSParser: infertype
 import ..ASACGraph
 import ..SimpleNeuron: AbstractNeuron
 
-validcols = [
-    :exterior_color,
-    :interior_color,
-    :drivetrain,
-    :fuel_type,
-    :transmission,
-    :engine,
-    :mileage,
-    :seller,
-    :reviews_count,
-    :seller_state,
-    :new_used,
-    :brand,
-    :comfort,
-    :interior_design,
-    :performance,
-    :value_for_the_money,
-    :exterior_styling,
-    :reliability,
-    :price,
-    :rating,
-    :year,
-    :model,
-    :features
-]
+function predict(magds::MAGDSSimple.Graph, sample::DataFrameRow, target::Symbol; validcols=[])
+	MAGDSSimple.deactivate!(magds)
 
-function predict(magds::MAGDSSimple.Graph, sample::DataFrameRow; validcols=validcols)
-    MAGDSSimple.deactivate!(magds)
-
-    validcols = Set(validcols)
-    if :price in validcols
-        pop!(validcols, :price)
-    end
     colnames = Symbol.(names(sample))
-    validcols = collect(intersect(validcols, colnames))
-    sample = select!(DataFrame(sample), validcols)
-
-    outneurons = Set{AbstractNeuron}()
-    for featurename in colnames
-        value = sample[1, featurename]
-        println("featurename $featurename value $value")
-        if !ismissing(value)
-            coltype, _ = infertype(typeof(value))
-            if typeof(value) <: AbstractArray
-                for el in value
-                    if !ismissing(el)
-                        sensor = ASACGraph.search(magds.sensors[featurename], coltype(el))
-                        if isnothing(sensor)
-                            sensor = ASACGraph.insert!(graph.sensors[column], coltype(el)) #TODO: remove tmp sensor   
-                        end
-                        neurons = ASACGraph.activate!(sensor, 1.0, true, false)
-                        outneurons = union(outneurons, neurons)
-                    end
-                end
-            else
-                sensor = ASACGraph.search(magds.sensors[featurename], coltype(value))
-                if isnothing(sensor)
-                    sensor = ASACGraph.insert!(graph.sensors[column], coltype(value)) #TODO: remove tmp sensor        
-                end
-                neurons = ASACGraph.activate!(sensor, 1.0, true, false)
-                outneurons = union(outneurons, neurons)
-            end
-        end
+	validcols = if isempty(validcols)
+        Set(colnames)
+    else
+        Set(validcols)
     end
-    outneurons
+	if target in validcols
+		pop!(validcols, target)
+	end
+    validcols = collect(intersect(validcols, colnames))
+	sample = select!(DataFrame(sample), validcols)
+
+	outneurons = Set{AbstractNeuron}()
+	for featurename in colnames
+		value = sample[1, featurename]
+		if !ismissing(value)
+			coltype, _ = infertype(typeof(value))
+			if typeof(value) <: AbstractArray
+				for el in value
+					if !ismissing(el)
+						sensor = ASACGraph.search(magds.sensors[featurename], coltype(el))
+						if isnothing(sensor)
+							sensor = ASACGraph.insert!(magds.sensors[featurename], coltype(el)) #TODO: remove tmp sensor
+						end
+						neurons = ASACGraph.activate!(sensor, 1.0, true, false)
+						outneurons = union(outneurons, neurons)
+					end
+				end
+			else
+				sensor = ASACGraph.search(magds.sensors[featurename], coltype(value))
+				if isnothing(sensor)
+					sensor = ASACGraph.insert!(magds.sensors[featurename], coltype(value)) #TODO: remove tmp sensor
+				end
+				neurons = ASACGraph.activate!(sensor, 1.0, true, false)
+				outneurons = union(outneurons, neurons)
+			end
+		end
+	end
+	sortedneurons = sort(collect(outneurons), by=x -> x.activation, rev=true)
+	# winner takes all strategy
+	for conn in sortedneurons[1].out
+		if conn.to.parent.name == string(target)
+			return conn.to.key
+		end
+	end
+end
+
+function predictions(magds::MAGDSSimple.Graph, Xtest::DataFrame, target::Symbol)::Vector
+	ypred = []
+	for (i, sample) in enumerate(eachrow(Xtest))
+		i % 100 == 0 && println(i)
+        push!(ypred, Float64(predict(magds, sample, target)))
+	end
+    ypred
 end
 
 end
